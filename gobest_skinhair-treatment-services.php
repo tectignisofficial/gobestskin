3055b4cad683693cb929e574cbaf78d5b2850777
Govind Bavkar <tectignisofficial@gmail.com>
2022-11-20 20:22:44 +0530

    update

---
+++ gobest_skinhair-treatment-services.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Gobest Skin &amp; Hair Clinic – Advanced Skin/Hair Treatments In Pune</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Medicio - v4.9.1
+  * Template URL: https://bootstrapmade.com/medicio-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+      <div class="align-items-center d-none d-md-flex">
+        <i class="bi bi-clock"></i> Monday - Saturday, 8AM to 10PM
+      </div>
+      <div class="d-flex align-items-center">
+        <i class="bi bi-phone"></i> Call us now +1 5589 55488 55
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <!-- <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt=""></a>
+
+       <h1 class="logo me-auto"><a href="index.html">Medicio</a></h1> 
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto " href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#departments">Departments</a></li>
+          <li><a class="nav-link scrollto" href="#doctors">Doctors</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+     
+
+      <a href="#appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">Make an</span> Appointment</a>
+
+    </div>
+  </header> -->
+  
+  
+  <!-- End Header -->
+
+ <!-- ======= Banner Section ======= -->
+
+
+
+<!-- ======= Banner Section ======= -->
+
+  <main id="main">
+
+  <div class="col-xs-12 col-sm-12 col-md-12 padding-none">
+        <div class="booking_box booking_box-2">
+            <div class="container container-new ">
+                <div class="row">
+                    <div class="box_side_icon-2">
+                        <div role="form" class="wpcf7" id="wpcf7-f327-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+<form action="/oliva-clinic-treatment-services/#wpcf7-f327-o1" method="post" class="wpcf7-form init" data-status="init" siq_id="autopick_2629">
+<div style="display: none;">
+<input type="hidden" name="_wpcf7" value="327">
+<input type="hidden" name="_wpcf7_version" value="5.6.3">
+<input type="hidden" name="_wpcf7_locale" value="en_US">
+<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f327-o1">
+<input type="hidden" name="_wpcf7_container_post" value="0">
+<input type="hidden" name="_wpcf7_posted_data_hash" value="">
+</div>
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+<div class="contect_form1">
+<i class="fa fa-history"></i><p></p>
+<div class="request-text">
+Talk to <span>Our Experts</span>
+</div>
+</div>
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 call-back-design">
+<div class="contect_form1">
+<span class="fa fa-user"></span><span class="wpcf7-form-control-wrap" data-name="user-name"><input type="text" name="user-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name" required=""></span>
+</div>
+<div class="contect_form1">
+<span class="fa fa-phone"></span><span class="wpcf7-form-control-wrap" data-name="user-mobile"><input pattern="(6|7|8|9)\d{9}" type="tel" name="user-mobile" value="" size="40" maxlength="11" minlength="10" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" aria-invalid="false" placeholder="Valid Mobile Number*" required=""></span>
+</div>
+<div class="contect_form1">
+<span class="fa fa-map-marker"></span><span class="wpcf7-form-control-wrap" data-name="select-city"><select name="select-city" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false" required=""><option value="">Select City*</option><option value="Bangalore">Bangalore</option><option value="Chennai">Chennai</option><option value="Hyderabad">Hyderabad</option><option value="Kochi">Kochi</option><option value="Kolkata">Kolkata</option><option value="Pune">Pune</option><option value="Vizag">Vizag</option><option value="Others">Others</option></select></span>
+</div>
+<div class="contect_form1">
+<span class="fa fa-map-marker"></span><span class="wpcf7-form-control-wrap" data-name="select-service"><select name="select-service" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false" required=""><option value="">Select Service*</option><optgroup label="Unwanted Hair"><option value="Unwanted Hair">Permanent Laser Hair Removal</option></optgroup><optgroup label="Skin"><option value="Pimple Treatment">Pimple Treatment</option><option value="Acne Scar Treatment">Acne Scar Treatment</option><option value="Skin Whitening">Skin Whitening</option><option value="Stretch marks treatment">Stretch marks treatment</option><option value="Tattoo">Tattoo Removal Treatment</option><option value="Pigmentation">Pigmentation Treatment</option><option value="Dull Skin">Dull Skin Treatment</option><option value="AntiAgeing">Anti Aging</option><option value="Botox">Botox Treatment</option><option value="Fillers">Fillers Treatment</option><option value="Moles">Moles Removal</option><option value="Warts">Warts Removal</option></optgroup><optgroup label="Hair"><option value="Hair Loss Treatment">Hair Loss Treatment</option><option value="Hair Transplantation">Hair Transplantation</option></optgroup><optgroup label="Weight Loss"><option value="Inch Loss Treatment">Fat Loss/Inch Loss</option><option value="Body Contouring">Body Contouring</option><option value="Weight Loss">Weight Loss Treatment</option><option value="Weight Management">Weight Management Program</option></optgroup><option value="Other">Others</option></select></span>
+</div>
+<div class="contect_btn">
+<ul>
+<input type="hidden" name="lead_source" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="sub_source" value="Email" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="utm_source" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="utm_medium" value="0" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="utm_campaign" value="0" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="gclid" value="undefined" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="cf7_title" value="Want a callback Desktop" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="cf7_date_time" value="201122194140282" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="cf7_web_url" value="https://www.olivaclinic.com/oliva-clinic-treatment-services/" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="prev_url" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
+<input type="hidden" name="sub_line_1" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
+<p></p>
+<li><input type="submit" value="Request a Call Back" class="wpcf7-form-control has-spinner wpcf7-submit" required=""><span class="wpcf7-spinner"></span></li>
+</ul>
+</div>
+</div>
+<div class="wpcf7-response-output" aria-hidden="true"></div></form></div>                        <!--pop-up-->
+                        <div class="call-on-phone" type="button" data-toggle="modal" data-target="#myModal-2">
+                            
+                            <a href="javascript:void(0);" class="call_now_button">Click Here</a>
+                        </div>
+
+                    </div>
+                    <!--pop-up-->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h1>Gobest Skin, Hair & Weight Treatment Services</h1>
+          
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon"><i class="fas fa-heartbeat"></i></div>
+            <h4 class="title"><a href="">Laser Hair Removal</a></h4>
+            <p class="description">Effective Solution To Get Hair-Free And Supple Skin</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon"><i class="fas fa-pills"></i></div>
+            <h4 class="title"><a href="">Pimples Treatment</a></h4>
+            <p class="description">Clinically-Proven Solutions To Clear Acne </p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon"><i class="fas fa-hospital-user"></i></div>
+            <h4 class="title"><a href="">Acne Scar Treatment</a></h4>
+            <p class="description">Best Professional Solutions To Reduce Acne Scars And Get Spotless Skin!</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon"><i class="fas fa-dna"></i></div>
+            <h4 class="title"><a href="">Hair loss treatment</a></h4>
+            <p class="description">Reverse Early Signs of Balding With Hair Regrowth Treatment</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon"><i class="fas fa-wheelchair"></i></div>
+            <h4 class="title"><a href="">Skin Lightening Treatment</a></h4>
+            <p class="description">FDA-approved Solutions to Get A Brighter Complexion and An Even Skin Tone! </p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon"><i class="fas fa-notes-medical"></i></div>
+            <h4 class="title"><a href="">Pigmentation Treatments</a></h4>
+            <p class="description">Get Rid Of Skin Discolouration And Pigmentation </p>
+          </div>
+         
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+   
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>Medicio</h3>
+              <p>
+                A108 Adam Street <br>
+                NY 535022, USA<br><br>
+                <strong>Phone:</strong> +1 5589 55488 55<br>
+                <strong>Email:</strong> info@example.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Medicio</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medicio-free-bootstrap-theme/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
