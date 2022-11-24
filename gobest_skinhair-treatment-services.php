294db53fbb3d5fea0d92490a34ab450c11e225e1
monikagore <mobnikgore5=6@m=gmail.com>
2022-11-24 15:14:15 +0530

    ch

--- gobest_skinhair-treatment-services.php
+++ gobest_skinhair-treatment-services.php
@@ -14,7 +14,9 @@
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
@@ -53,96 +55,172 @@
 
   <!-- ======= Header ======= -->
   <?php include("include/header.php");?>
-  
-  
+
+
   <!-- End Header -->
 
- <!-- ======= Banner Section ======= -->
+  <!-- ======= Banner Section ======= -->
 
 
 
-<!-- ======= Banner Section ======= -->
+  <!-- ======= Banner Section ======= -->
 
   <main id="main">
 
-  <div class="col-xs-12 col-sm-12 col-md-12 padding-none">
-        <div class="booking_box booking_box-2">
-            <div class="container container-new ">
-                <div class="row">
-                    <div class="box_side_icon-2">
-                        <div role="form" class="wpcf7" id="wpcf7-f327-o1" lang="en-US" dir="ltr">
-<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
-<form action="/oliva-clinic-treatment-services/#wpcf7-f327-o1" method="post" class="wpcf7-form init" data-status="init" siq_id="autopick_2629">
-<div style="display: none;">
-<input type="hidden" name="_wpcf7" value="327">
-<input type="hidden" name="_wpcf7_version" value="5.6.3">
-<input type="hidden" name="_wpcf7_locale" value="en_US">
-<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f327-o1">
-<input type="hidden" name="_wpcf7_container_post" value="0">
-<input type="hidden" name="_wpcf7_posted_data_hash" value="">
-</div>
-<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-<div class="contect_form1">
-<i class="fa fa-history"></i><p></p>
-<div class="request-text">
-Talk to <span>Our Experts</span>
-</div>
-</div>
-</div>
-<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 call-back-design">
-<div class="contect_form1">
-<span class="fa fa-user"></span><span class="wpcf7-form-control-wrap" data-name="user-name"><input type="text" name="user-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name" required=""></span>
-</div>
-<div class="contect_form1">
-<span class="fa fa-phone"></span><span class="wpcf7-form-control-wrap" data-name="user-mobile"><input pattern="(6|7|8|9)\d{9}" type="tel" name="user-mobile" value="" size="40" maxlength="11" minlength="10" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" aria-invalid="false" placeholder="Valid Mobile Number*" required=""></span>
-</div>
-<div class="contect_form1">
-<span class="fa fa-map-marker"></span><span class="wpcf7-form-control-wrap" data-name="select-city"><select name="select-city" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false" required=""><option value="">Select City*</option><option value="Bangalore">Bangalore</option><option value="Chennai">Chennai</option><option value="Hyderabad">Hyderabad</option><option value="Kochi">Kochi</option><option value="Kolkata">Kolkata</option><option value="Pune">Pune</option><option value="Vizag">Vizag</option><option value="Others">Others</option></select></span>
-</div>
-<div class="contect_form1">
-<span class="fa fa-map-marker"></span><span class="wpcf7-form-control-wrap" data-name="select-service"><select name="select-service" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false" required=""><option value="">Select Service*</option><optgroup label="Unwanted Hair"><option value="Unwanted Hair">Permanent Laser Hair Removal</option></optgroup><optgroup label="Skin"><option value="Pimple Treatment">Pimple Treatment</option><option value="Acne Scar Treatment">Acne Scar Treatment</option><option value="Skin Whitening">Skin Whitening</option><option value="Stretch marks treatment">Stretch marks treatment</option><option value="Tattoo">Tattoo Removal Treatment</option><option value="Pigmentation">Pigmentation Treatment</option><option value="Dull Skin">Dull Skin Treatment</option><option value="AntiAgeing">Anti Aging</option><option value="Botox">Botox Treatment</option><option value="Fillers">Fillers Treatment</option><option value="Moles">Moles Removal</option><option value="Warts">Warts Removal</option></optgroup><optgroup label="Hair"><option value="Hair Loss Treatment">Hair Loss Treatment</option><option value="Hair Transplantation">Hair Transplantation</option></optgroup><optgroup label="Weight Loss"><option value="Inch Loss Treatment">Fat Loss/Inch Loss</option><option value="Body Contouring">Body Contouring</option><option value="Weight Loss">Weight Loss Treatment</option><option value="Weight Management">Weight Management Program</option></optgroup><option value="Other">Others</option></select></span>
-</div>
-<div class="contect_btn">
-<ul>
-<input type="hidden" name="lead_source" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="sub_source" value="Email" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="utm_source" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="utm_medium" value="0" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="utm_campaign" value="0" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="gclid" value="undefined" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="cf7_title" value="Want a callback Desktop" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="cf7_date_time" value="201122194140282" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="cf7_web_url" value="https://www.olivaclinic.com/oliva-clinic-treatment-services/" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="prev_url" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
-<input type="hidden" name="sub_line_1" value="SEO" class="wpcf7-form-control wpcf7-hidden" required="">
-<p></p>
-<li><input type="submit" value="Request a Call Back" class="wpcf7-form-control has-spinner wpcf7-submit" required=""><span class="wpcf7-spinner"></span></li>
-</ul>
-</div>
-</div>
-<div class="wpcf7-response-output" aria-hidden="true"></div></form></div>                        <!--pop-up-->
-                        <div class="call-on-phone" type="button" data-toggle="modal" data-target="#myModal-2">
-                            
-                            <a href="javascript:void(0);" class="call_now_button">Click Here</a>
-                        </div>
-
-                    </div>
-                    <!--pop-up-->
+    <div class="col-xs-12 col-sm-12 col-md-12 padding-none">
+      <div class="booking_box booking_box-2">
+        <div class="container container-new ">
+          <div class="row">
+            <div class="box_side_icon-2">
+              <div role="form" class="wpcf7" id="wpcf7-f327-o1" lang="en-US" dir="ltr">
+                <div class="screen-reader-response">
+                  <p role="status" aria-live="polite" aria-atomic="true"></p>
+                  <ul></ul>
                 </div>
+                <form action="/oliva-clinic-treatment-services/#wpcf7-f327-o1" method="post" class="wpcf7-form init"
+                  data-status="init" siq_id="autopick_2629">
+                  <div style="display: none;">
+                    <input type="hidden" name="_wpcf7" value="327">
+                    <input type="hidden" name="_wpcf7_version" value="5.6.3">
+                    <input type="hidden" name="_wpcf7_locale" value="en_US">
+                    <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f327-o1">
+                    <input type="hidden" name="_wpcf7_container_post" value="0">
+                    <input type="hidden" name="_wpcf7_posted_data_hash" value="">
+                  </div>
+                  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <div class="contect_form1">
+                      <i class="fa fa-history"></i>
+                      <p></p>
+                      <div class="request-text">
+                        Talk to <span>Our Experts</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 call-back-design">
+                    <div class="contect_form1">
+                      <span class="fa fa-user"></span><span class="wpcf7-form-control-wrap" data-name="user-name"><input
+                          type="text" name="user-name" value="" size="40"
+                          class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true"
+                          aria-invalid="false" placeholder="Name" required=""></span>
+                    </div>
+                    <div class="contect_form1">
+                      <span class="fa fa-phone"></span><span class="wpcf7-form-control-wrap"
+                        data-name="user-mobile"><input pattern="(6|7|8|9)\d{9}" type="tel" name="user-mobile" value=""
+                          size="40" maxlength="11" minlength="10"
+                          class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel"
+                          aria-required="true" aria-invalid="false" placeholder="Valid Mobile Number*"
+                          required=""></span>
+                    </div>
+                    <div class="contect_form1">
+                      <span class="fa fa-map-marker"></span><span class="wpcf7-form-control-wrap"
+                        data-name="select-city"><select name="select-city"
+                          class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true"
+                          aria-invalid="false" required="">
+                          <option value="">Select City*</option>
+                          <option value="Bangalore">Bangalore</option>
+                          <option value="Chennai">Chennai</option>
+                          <option value="Hyderabad">Hyderabad</option>
+                          <option value="Kochi">Kochi</option>
+                          <option value="Kolkata">Kolkata</option>
+                          <option value="Pune">Pune</option>
+                          <option value="Vizag">Vizag</option>
+                          <option value="Others">Others</option>
+                        </select></span>
+                    </div>
+                    <div class="contect_form1">
+                      <span class="fa fa-map-marker"></span><span class="wpcf7-form-control-wrap"
+                        data-name="select-service"><select name="select-service"
+                          class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true"
+                          aria-invalid="false" required="">
+                          <option value="">Select Service*</option>
+                          <optgroup label="Unwanted Hair">
+                            <option value="Unwanted Hair">Permanent Laser Hair Removal</option>
+                          </optgroup>
+                          <optgroup label="Skin">
+                            <option value="Pimple Treatment">Pimple Treatment</option>
+                            <option value="Acne Scar Treatment">Acne Scar Treatment</option>
+                            <option value="Skin Whitening">Skin Whitening</option>
+                            <option value="Stretch marks treatment">Stretch marks treatment</option>
+                            <option value="Tattoo">Tattoo Removal Treatment</option>
+                            <option value="Pigmentation">Pigmentation Treatment</option>
+                            <option value="Dull Skin">Dull Skin Treatment</option>
+                            <option value="AntiAgeing">Anti Aging</option>
+                            <option value="Botox">Botox Treatment</option>
+                            <option value="Fillers">Fillers Treatment</option>
+                            <option value="Moles">Moles Removal</option>
+                            <option value="Warts">Warts Removal</option>
+                          </optgroup>
+                          <optgroup label="Hair">
+                            <option value="Hair Loss Treatment">Hair Loss Treatment</option>
+                            <option value="Hair Transplantation">Hair Transplantation</option>
+                          </optgroup>
+                          <optgroup label="Weight Loss">
+                            <option value="Inch Loss Treatment">Fat Loss/Inch Loss</option>
+                            <option value="Body Contouring">Body Contouring</option>
+                            <option value="Weight Loss">Weight Loss Treatment</option>
+                            <option value="Weight Management">Weight Management Program</option>
+                          </optgroup>
+                          <option value="Other">Others</option>
+                        </select></span>
+                    </div>
+                    <div class="contect_btn">
+                      <ul>
+                        <input type="hidden" name="lead_source" value="SEO" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="sub_source" value="Email" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="utm_source" value="SEO" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="utm_medium" value="0" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="utm_campaign" value="0" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="gclid" value="undefined" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="cf7_title" value="Want a callback Desktop"
+                          class="wpcf7-form-control wpcf7-hidden" required="">
+                        <input type="hidden" name="cf7_date_time" value="201122194140282"
+                          class="wpcf7-form-control wpcf7-hidden" required="">
+                        <input type="hidden" name="cf7_web_url"
+                          value="https://www.olivaclinic.com/oliva-clinic-treatment-services/"
+                          class="wpcf7-form-control wpcf7-hidden" required="">
+                        <input type="hidden" name="prev_url" value="SEO" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <input type="hidden" name="sub_line_1" value="SEO" class="wpcf7-form-control wpcf7-hidden"
+                          required="">
+                        <p></p>
+                        <li><input type="submit" value="Request a Call Back"
+                            class="wpcf7-form-control has-spinner wpcf7-submit" required=""><span
+                            class="wpcf7-spinner"></span></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="wpcf7-response-output" aria-hidden="true"></div>
+                </form>
+              </div>
+              <!--pop-up-->
+              <div class="call-on-phone" type="button" data-toggle="modal" data-target="#myModal-2">
+
+                <a href="javascript:void(0);" class="call_now_button">Click Here</a>
+              </div>
+
             </div>
+            <!--pop-up-->
+          </div>
         </div>
+      </div>
     </div>
 
 
 
-    
+
     <!-- ======= Services Section ======= -->
     <section id="services" class="services services">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
           <h1>Gobest Skin, Hair & Weight Treatment Services</h1>
-          
+
         </div>
 
         <div class="row">
@@ -176,13 +254,13 @@ Talk to <span>Our Experts</span>
             <h4 class="title"><a href="">Pigmentation Treatments</a></h4>
             <p class="description">Get Rid Of Skin Discolouration And Pigmentation </p>
           </div>
-         
+
         </div>
 
       </div>
     </section><!-- End Services Section -->
 
-   
+
 
   </main><!-- End #main -->
 
@@ -191,7 +269,8 @@ Talk to <span>Our Experts</span>
   <!-- End Footer -->
 
   <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
