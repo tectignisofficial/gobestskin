df7fee9cab8234be2d71b8d7513b1c4ef673f06d
keshridivya <dkeshari094@gmail.com>
2022-11-25 17:03:16 +0530

    laserhair

---
+++ laser-hair-removal-treatment.php
@@ -0,0 +1,351 @@
+<?php 
+include("include/config.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Gobest Skin &amp; Hair Clinic – Advanced Skin/Hair Treatments In Pune</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <style>
+        #hero {
+            height: 70vh;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- ======= Top Bar ======= -->
+    <div id="topbar" class="d-flex align-items-center fixed-top">
+        <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+            <div class="align-items-center d-none d-md-flex">
+                <i class="bi bi-clock"></i> Monday - Saturday, 8AM to 10PM
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="bi bi-phone"></i> Call us now +1 5589 55488 55
+            </div>
+        </div>
+    </div>
+
+    <!-- ======= Header ======= -->
+    <?php include("include/header.php");?>
+
+
+    <!-- End Header -->
+
+
+
+    <main id="main">
+
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Laser hair Removal treatment</h2>
+
+                </div>
+
+            </div>
+
+            <div>
+                <img src="assets/img/laser-hair-removal.jpg" alt="" style="width: -webkit-fill-available;">
+            </div>
+
+            <div class="container">
+                <div class="row mt-5">
+
+                    <div class="col-12">
+                        <div class="breadcrumb">
+                            <ul>
+                                <li><i class="fa fa-home"></i> <a href="index.php" title="Home"><span>Home</span></a>
+                                </li>
+                                <li><i class="fa fa-caret-right"></i> <span title="Laser Hair Removal">Laser Hair
+                                        Removal</span></li>
+                            </ul>
+                        </div>
+                        <div class="row">
+
+                            <h2>Laser Treatment For Body Hair Removal</h2>
+                            <p class="treatment-subtitle"><strong>Effective Solution To Get Hair-Free And Supple
+                                    Skin</strong></p>
+                            <div class="col-12 col-md-8 col-lg-8">
+                                <p>The term “laser” is an acronym for Light Amplification by Stimulated Emission of
+                                    Radiation. Laser hair removal (LHR) is an advanced aesthetic treatment that uses
+                                    a high-energy beam of light in a focussed manner to destroy stem cells found in
+                                    hair follicles systematically. This eradicates the possibility of new hair
+                                    growth and delivers long-lasting results. The innovative use of laser technology
+                                    for hair removal offers a high degree of precision, causing no damage to the
+                                    surrounding skin.</p>
+
+                                <div>
+                                    <h5>What Are The Benefits Of Laser Hair Removal Service?</h5>
+                                    <p>Laser Hair Removal (LHR) procedure at Oliva is a skin investment that you
+                                        make today that pays you rich dividends for life! At Oliva, LHR is a safe,
+                                        painless and holistic treatment to eliminate hair growth that causes zero
+                                        damage to your skin. The results are permanent and provide a practical and
+                                        cost-effective alternative to waxing and shaving. Other benefits of laser
+                                        hair removal are as follows:</p>
+                                    <ul>
+                                        <li>Offers permanent hair reduction</li>
+                                        <li>Suitable for hair removal from any area of the body</li>
+                                        <li>A quick procedure that lasts for 45 minutes to an hour</li>
+                                        <li>Zero downtime</li>
+                                        <li>No significant side effects</li>
+                                        <li>A hassle-free method that is cost-effective in the long run</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5>Is LHR A Safe Treatment For All Areas Of The Body</h5>
+                                    <p>The advanced laser machines at our clinic enable us to perform the hair
+                                        removal treatment on all Indian skin types. We treat different areas of the
+                                        body for both men and women based on their specific grooming needs. We offer
+                                        the following packages:</p>
+                                    <ul>
+                                        <li>Full body hair removal</li>
+                                        <li>Facial hair removal</li>
+                                        <li>Underarm hair removal</li>
+                                        <li>Bikini (private part/pubic) hair removal</li>
+                                        <li>Upper lip and chin hair removal</li>
+                                        <li>Leg hair removal</li>
+                                        <li>Chest, abdomen and back hair removal</li>
+                                        <li>Arms and upper body hair removal</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5>What Are The Steps Of LHR Treatment Procedure?</h5>
+                                    <p>Here are the steps you can expect during a session of LHR treatment at Oliva:
+                                    </p>
+                                    <ul>
+                                        <li>Trained therapists will shave the target area and apply a cooling gel
+                                            before the laser treatment.</li>
+                                        <li>An experienced doctor will customise the parameters of the laser beam
+                                            according to your skin and hair type.</li>
+                                        <li>The therapist will divide the target area into sections.</li>
+                                        <li>The dermatologist will use the handheld laser device over the target
+                                            area to eliminate the unwanted hair follicles.</li>
+                                        <li>At the end of the procedure, the therapist will apply a cold compress to
+                                            alleviate any discomfort or redness.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5>Are You A Suitable Candidate For Laser Hair Removal?</h5>
+                                    <p>If you are somebody who wants to get rid of unwanted hair forever, then you
+                                        are the right candidate for laser hair removal treatment at Oliva Clinic.
+                                        This procedure is suitable for all skin types, notably lighter skin tone and
+                                        darker hair.</p>
+                                    <p>However, it is not advisable for pregnant and lactating women and young
+                                        adults (undergoing puberty) as they may be experiencing significant hormonal
+                                        changes during these stages.</p>
+                                </div>
+                                <div>
+                                    <h5>What Is The Cost Of Laser Hair Removal Treatment?</h5>
+                                    <p>The cost of laser hair removal treatment in India starts at Rs 1,600* per
+                                        session and may range up to Rs 30,000* per session. The price might vary for
+                                        individuals depending on factors like the size of the target area, the hair
+                                        colour, type and density, the skin tone and the underlying cause of hair
+                                        growth that determine the suitable laser technology and the number of
+                                        sessions required. For example, upper lip hair removal is less expensive as
+                                        compared to full-body hair removal.</p>
+                                    <p style="font-weight:bold;">* T&C Apply. The average prices mentioned here are
+                                        approximate and indicative ONLY and are subject to revision based on your
+                                        individual hair removal needs. If you would like to know the exact cost for
+                                        the laser hair removal procedure, it is advisable to consult with our
+                                        dermatologists and attain a customised treatment plan. This information is
+                                        an estimate only and is in no way a final quote or a fixed price of laser
+                                        hair removal treatment at Oliva.</p>
+                                </div>
+                                <div>
+                                    <h5>How To Get Started With The LHR Treatment At Oliva?</h5>
+                                    <p>You can book an appointment online or over the phone by dialling
+                                        1800-103-3893. Our customer care team will schedule an appointment with an
+                                        expert dermatologist at your preferred time and convenience.</p><br>
+                                </div>
+
+                            </div>
+
+                            <div class="col-12 col-md-4 col-lg-4 text-center" >
+                                <div style="border-left: 1px solid #d8d6d6;padding-left:30px">
+                                    <section id="hero">
+                                        <div id="heroCarousel" data-bs-interval="5000"
+                                            class="carousel slide carousel-fade" data-bs-ride="carousel">
+                                            <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+                                            <div class="carousel-inner" role="listbox">
+                                                <!-- Slide 1 -->
+                                                <div class="carousel-item active"
+                                                    style="background-image: url(assets/img/slide/lasercaro1.webp)">
+                                                </div>
+                                                <!-- Slide 2 -->
+                                                <div class="carousel-item"
+                                                    style="background-image: url(assets/img/slide/lasercaro2.webp)">
+                                                </div>
+                                                <!-- Slide 3 -->
+                                                <div class="carousel-item"
+                                                    style="background-image: url(assets/img/slide/lasercaro3.webp)">
+                                                </div>
+                                                <!-- Slide 4 -->
+                                                <div class="carousel-item"
+                                                    style="background-image: url(assets/img/slide/lasercaro4.webp)">
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--video-->
+            <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>In an emergency? Need help now?</h3>
+          <p> OUR 91% CLIENT SATISFACTION RATE IS UNPARALLELED IN OUR FIELD.</p>
+          <a class="cta-btn scrollto" href="#appointment">Make an Make an Appointment</a>
+        </div>
+
+      </div>
+    </section>
+    <!--end video-->
+
+        </section><!-- End Contact Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include("include/footer.php");?>
+    <!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            if ($(".comparison-slider")[0]) {
+                let compSlider = $(".comparison-slider");
+                compSlider.each(function () {
+                    let compSliderWidth = $(this).width() + "px";
+                    $(this).find(".resize img").css({
+                        width: compSliderWidth
+                    });
+                    drags($(this).find(".divider"), $(this).find(".resize"), $(this));
+                });
+                $(window).on("resize", function () {
+                    let compSliderWidth = compSlider.width() + "px";
+                    compSlider.find(".resize img").css({
+                        width: compSliderWidth
+                    });
+                });
+            }
+        });
+
+        function drags(dragElement, resizeElement, container) {
+
+            let touched = false;
+            window.addEventListener('touchstart', function () {
+                touched = true;
+            });
+            window.addEventListener('touchend', function () {
+                touched = false;
+            });
+            dragElement.on("mousedown touchstart", function (e) {
+                dragElement.addClass("draggable");
+                resizeElement.addClass("resizable");
+                //create vars
+                let startX = e.pageX ? e.pageX : e.originalEvent.touches[0].pageX;
+                let dragWidth = dragElement.outerWidth();
+                let posX = dragElement.offset().left + dragWidth - startX;
+                let containerOffset = container.offset().left;
+                let containerWidth = container.outerWidth();
+                let minLeft = containerOffset + 10;
+                let maxLeft = containerOffset + containerWidth - dragWidth - 10;
+                dragElement.parents().on("mousemove touchmove", function (e) {
+
+                    if (touched === false) {
+                        e.preventDefault();
+                    }
+
+                    let moveX = e.pageX ? e.pageX : e.originalEvent.touches[0].pageX;
+                    let leftValue = moveX + posX - dragWidth;
+
+                    if (leftValue < minLeft) {
+                        leftValue = minLeft;
+                    } else if (leftValue > maxLeft) {
+                        leftValue = maxLeft;
+                    }
+
+                    let widthValue = (leftValue + dragWidth / 2 - containerOffset) * 100 /
+                        containerWidth + "%";
+
+                    $(".draggable").css("left", widthValue).on("mouseup touchend touchcancel",
+                        function () {
+                            $(this).removeClass("draggable");
+                            resizeElement.removeClass("resizable");
+                        });
+
+                    $(".resizable").css("width", widthValue);
+
+                }).on("mouseup touchend touchcancel", function () {
+                    dragElement.removeClass("draggable");
+                    resizeElement.removeClass("resizable");
+
+                });
+
+            }).on("mouseup touchend touchcancel", function (e) {
+                // stop clicping the image and move the slider
+                dragElement.removeClass("draggable");
+                resizeElement.removeClass("resizable");
+
+            });
+
+        }
+    </script>
+</body>
+
+</html>
